e9b064281252a1b3b606dd8d90093875220beaab
Moody Salem <moody.salem@gmail.com>
2021-02-23 15:52:04 -0600

    721 must implement 165

--- contracts/interfaces/external/IERC721.sol
+++ contracts/interfaces/external/IERC721.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0;
+pragma solidity >=0.6.2;
+
+import './IERC165.sol';
 
 /// @title ERC-721 Non-Fungible Token Standard
 /// @dev See https://eips.ethereum.org/EIPS/eip-721
 ///  Note: the ERC-165 identifier for this interface is 0x80ac58cd.
-/* is ERC165 */
-interface IERC721 {
+interface IERC721 is IERC165 {
     /// @dev This emits when ownership of any NFT changes by any mechanism.
     ///  This event emits when NFTs are created (`from` == 0) and destroyed
     ///  (`to` == 0). Exception: during contract creation, any number of NFTs
