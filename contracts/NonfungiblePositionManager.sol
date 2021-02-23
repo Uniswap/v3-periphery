54ed796d1ccab6e8768448ea3badf37f5b6c6486
Moody Salem <moody.salem@gmail.com>
2021-02-23 16:03:50 -0600

    start implementing the interface

--- contracts/NonfungiblePositionManager.sol
+++ contracts/NonfungiblePositionManager.sol
@@ -1,8 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity =0.7.6;
+pragma abicoder v2;
 
-import './interfaces/IRouterImmutableState.sol';
-import './RouterValidation.sol';
 import './interfaces/INonfungiblePositionManager.sol';
+import './RouterPositions.sol';
 
-abstract contract NonfungiblePositionManager is IRouterImmutableState, INonfungiblePositionManager, RouterValidation {}
+abstract contract NonfungiblePositionManager is INonfungiblePositionManager, RouterPositions {
+    /// @inheritdoc IERC721Metadata
+    string public constant override name = 'Uniswap V3 Positions';
+    /// @inheritdoc IERC721Metadata
+    string public constant override symbol = 'UNI-V3-P';
+
+    /// @inheritdoc IERC721Metadata
+    function tokenURI(uint256 _tokenId) external view override returns (string memory) {
+        return string(abi.encode('data:text/plain,'));
+    }
+}
--- contracts/interfaces/INonfungiblePositionManager.sol
+++ contracts/interfaces/INonfungiblePositionManager.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.5.0;
 
+import './external/IERC721Metadata.sol';
+
 /// @title Non-fungible position manager
 /// @notice Non-fungible token wrapper for Uniswap V3 Positions
-interface INonfungiblePositionManager {
+interface INonfungiblePositionManager is IERC721Metadata {
 
 }
