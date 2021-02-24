e878c848d371811db9a80de4ce7a4b72e5c78086
Georgios Konstantopoulos <me@gakonst.com>
2021-02-24 16:52:14 +0200

    feat: add library for calculating liquidity from amount0/amount1

---
+++ contracts/libraries/FixedPoint96.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.4.0;
+
+/// @title FixedPoint96
+/// @notice A library for handling binary fixed point numbers, see https://en.wikipedia.org/wiki/Q_(number_format)
+/// @dev Used in SqrtPriceMath.sol
+library FixedPoint96 {
+    uint8 internal constant RESOLUTION = 96;
+    uint256 internal constant Q96 = 0x1000000000000000000000000;
+}
