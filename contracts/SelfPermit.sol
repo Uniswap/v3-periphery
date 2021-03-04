5238c31363efa7532362cb5aef1ce2b0a6430dc5
Noah Zinsmeister <noahwz@gmail.com>
2021-03-04 08:56:22 -0600

    add some natspec

--- contracts/SelfPermit.sol
+++ contracts/SelfPermit.sol
@@ -3,7 +3,12 @@ pragma solidity >=0.5.0;
 
 import './interfaces/external/IERC2612.sol';
 
+/// @title Self Permit
+/// @notice Functionality to call permit on any EIP-2612-compliant token for use in the route
 abstract contract SelfPermit {
+    /// @notice The permit `owner` is always msg.sender, as this is a safety condition for now 
+    /// @dev The permit `owner` is always msg.sender, as this is a safety condition for now 
+    /// @dev The permit `spender` is always address(this)
     function selfPermit(
         address token,
         uint256 value,
@@ -12,8 +17,6 @@ abstract contract SelfPermit {
         bytes32 r,
         bytes32 s
     ) external {
-        // owner should always be msg.sender for now
-        // spender is always address(this)
         IERC2612(token).permit(msg.sender, address(this), value, deadline, v, r, s);
     }
 }
