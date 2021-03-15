276ea45a61a5f1db36e4b68e30b61ee7238bf4d8
Noah Zinsmeister <noahwz@gmail.com>
2021-03-15 19:12:57 -0400

    add tick lens

---
+++ contracts/TickLens.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.5.0;
+pragma abicoder v2;
+
+import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
+
+/// @title Tick Lens contract
+contract TickLens {
+    int24 private constant MIN_TICK = -887272;
+    int24 private constant MAX_TICK = -MIN_TICK;
+
+    struct TickInfo {
+        int24 tick;
+        int128 liquidityNet;
+        uint128 liquidityGross;
+    }
+
+    function getPopulatedTicks(address pool, int24 tickLower, int24 tickUpper)
+        external
+        view
+        returns (TickInfo[] memory ticks)
+    {
+        require(tickLower < tickUpper);
+
+        int24 tickSpacing = IUniswapV3Pool(pool).tickSpacing();
+
+        int16 indexLower = int16((tickLower / tickSpacing) >> 8);
+        int16 indexUpper = int16((tickUpper / tickSpacing) >> 8);
+
+        uint256 numberOfBitmaps = uint256(int256(indexUpper) - indexLower + 1);
+        uint256[] memory bitmaps = new uint256[](numberOfBitmaps);
+
+        // fetch all bitmaps
+        for (int256 i = indexLower; i <= indexUpper; i++) {
+            bitmaps[uint256(i - indexLower)] = IUniswapV3Pool(pool).tickBitmap(int16(i));
+        }
+
+        // fetch the number of populated ticks
+        uint256 numberOfPopulatedTicks;
+        for (uint256 i = 0; i < numberOfBitmaps; i++) {
+            uint256 bitmap = bitmaps[i];
+            for (uint256 j = 0; j < 256; j++) {
+                if (bitmap & (1 << j) > 0) numberOfPopulatedTicks++;
+            }
+        }
+
+        // return populated ticks
+        ticks = new TickInfo[](numberOfPopulatedTicks);
+        for (int256 i = indexLower; i <= indexUpper; i++) {
+            uint256 bitmap = bitmaps[uint256(i - indexLower)];
+            for (uint256 j = 0; j < 256; j++) {
+                if (bitmap & (1 << j) > 0) {
+                    int24 tick = int24(((i << 8) + int256(j)) * tickSpacing);
+                    (uint128 liquidityGross, int128 liquidityNet, , ) = IUniswapV3Pool(pool).ticks(tick);
+                    ticks[--numberOfPopulatedTicks] = TickInfo({
+                        tick: tick,
+                        liquidityNet: liquidityNet,
+                        liquidityGross: liquidityGross
+                    });
+                }
+            }
+        }
+    }
+}
---
+++ test/TickLens.spec.ts
@@ -0,0 +1,177 @@
+import { Fixture } from 'ethereum-waffle'
+import { constants, Contract } from 'ethers'
+import { ethers, waffle } from 'hardhat'
+import { MockTimeNonfungiblePositionManager, TestERC20, TickLens } from '../typechain'
+import completeFixture from './shared/completeFixture'
+import { FeeAmount, TICK_SPACINGS } from './shared/constants'
+import { encodePriceSqrt } from './shared/encodePriceSqrt'
+import { expect } from './shared/expect'
+import { getMaxTick, getMinTick } from './shared/ticks'
+import { computePoolAddress } from './shared/computePoolAddress'
+
+describe('Tick Lens', () => {
+  const wallets = waffle.provider.getWallets()
+
+  const nftFixture: Fixture<{
+    factory: Contract
+    nft: MockTimeNonfungiblePositionManager
+    tokens: [TestERC20, TestERC20, TestERC20]
+  }> = async (wallets, provider) => {
+    const { factory, tokens, nft } = await completeFixture(wallets, provider)
+
+    for (const token of tokens) {
+      await token.approve(nft.address, constants.MaxUint256)
+    }
+
+    return {
+      factory,
+      nft,
+      tokens,
+    }
+  }
+
+  let factory: Contract
+  let nft: MockTimeNonfungiblePositionManager
+  let tokens: [TestERC20, TestERC20, TestERC20]
+  let poolAddress: string
+  let tickLens: TickLens
+
+  let loadFixture: ReturnType<typeof waffle.createFixtureLoader>
+
+  before('create fixture loader', async () => {
+    loadFixture = waffle.createFixtureLoader(wallets)
+  })
+
+  beforeEach('load fixture', async () => {
+    ;({ factory, tokens, nft } = await loadFixture(nftFixture))
+  })
+
+  describe('#getPopulatedTicks', () => {
+    const liquidity = 1000000
+    async function createPool(tokenAddressA: string, tokenAddressB: string) {
+      if (tokenAddressA.toLowerCase() > tokenAddressB.toLowerCase())
+        [tokenAddressA, tokenAddressB] = [tokenAddressB, tokenAddressA]
+
+      const liquidityParams = {
+        token0: tokenAddressA,
+        token1: tokenAddressB,
+        fee: FeeAmount.MEDIUM,
+        sqrtPriceX96: encodePriceSqrt(1, 1),
+        tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+        tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+        recipient: wallets[0].address,
+        amount: liquidity,
+        deadline: 1,
+      }
+
+      return nft.firstMint(liquidityParams)
+    }
+
+    async function mint(
+      tokenAddressA: string,
+      tokenAddressB: string,
+      tickLower: number,
+      tickUpper: number,
+      amount: number
+    ) {
+      if (tokenAddressA.toLowerCase() > tokenAddressB.toLowerCase())
+        [tokenAddressA, tokenAddressB] = [tokenAddressB, tokenAddressA]
+
+      const liquidityParams = {
+        token0: tokenAddressA,
+        token1: tokenAddressB,
+        fee: FeeAmount.MEDIUM,
+        tickLower,
+        tickUpper,
+        amount,
+        amount0Max: constants.MaxUint256,
+        amount1Max: constants.MaxUint256,
+        recipient: wallets[0].address,
+        deadline: 1,
+      }
+
+      return nft.mint(liquidityParams)
+    }
+
+    beforeEach(async () => {
+      await createPool(tokens[0].address, tokens[1].address)
+      poolAddress = computePoolAddress(factory.address, [tokens[0].address, tokens[1].address], FeeAmount.MEDIUM)
+    })
+
+    beforeEach(async () => {
+      const lensFactory = await ethers.getContractFactory('TickLens')
+      tickLens = (await lensFactory.deploy()) as TickLens
+    })
+
+    it('works for min/max', async () => {
+      const [max, min] = await tickLens.getPopulatedTicks(
+        poolAddress,
+        getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+        getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM])
+      )
+
+      expect(min.tick).to.be.eq(getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]))
+      expect(min.liquidityNet).to.be.eq(liquidity)
+      expect(min.liquidityGross).to.be.eq(liquidity)
+      expect(max.tick).to.be.eq(getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]))
+      expect(max.liquidityNet).to.be.eq(liquidity * -1)
+      expect(min.liquidityGross).to.be.eq(liquidity)
+    })
+
+    it('works for min/max and -1/0/1', async () => {
+      const minus = -TICK_SPACINGS[FeeAmount.MEDIUM]
+      const plus = -minus
+
+      await mint(tokens[0].address, tokens[1].address, minus, 0, 1)
+      await mint(tokens[0].address, tokens[1].address, minus, plus, 3)
+      await mint(tokens[0].address, tokens[1].address, 0, plus, 5)
+
+      const [max, one, zero, negativeOne, min] = await tickLens.getPopulatedTicks(
+        poolAddress,
+        getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+        getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM])
+      )
+
+      console.log([max, one, zero, negativeOne, min])
+
+      expect(min.tick).to.be.eq(getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]))
+      expect(min.liquidityNet).to.be.eq(liquidity)
+      expect(min.liquidityGross).to.be.eq(liquidity)
+
+      expect(negativeOne.tick).to.be.eq(minus)
+      expect(negativeOne.liquidityNet).to.be.eq(3 + 1)
+      expect(negativeOne.liquidityGross).to.be.eq(4)
+
+      expect(zero.tick).to.be.eq(0)
+      expect(zero.liquidityNet).to.be.eq(5 - 1)
+      expect(zero.liquidityGross).to.be.eq(6)
+
+      expect(one.tick).to.be.eq(plus)
+      expect(one.liquidityNet).to.be.eq(-5 - 3)
+      expect(one.liquidityGross).to.be.eq(8)
+
+      expect(max.tick).to.be.eq(getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]))
+      expect(max.liquidityNet).to.be.eq(liquidity * -1)
+      expect(max.liquidityGross).to.be.eq(liquidity)
+    })
+
+    it.skip('works for every 50th tick', async () => {
+      const max = getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM])
+      const interval = 50
+      for (let tick = TICK_SPACINGS[FeeAmount.MEDIUM]; tick < max; tick += TICK_SPACINGS[FeeAmount.MEDIUM] * interval) {
+        console.log(`${((tick / max) * 100).toPrecision(4)}%`)
+        await mint(tokens[0].address, tokens[1].address, -tick, tick, 1)
+      }
+
+      const ticks = await tickLens.getPopulatedTicks(
+        poolAddress,
+        getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+        getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM])
+      )
+      for (let i = 0; i < ticks.length; i++) {
+        if (i > 0) expect(ticks[i].tick).to.be.lt(ticks[i - 1].tick)
+        expect(ticks[i].liquidityGross).to.be.gt(0)
+      }
+    }).timeout(60000 * 5)
+  })
+})
