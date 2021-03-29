f67eff5c70ce35d28a2aed0a3935e7765e0321f0
Noah Zinsmeister <noahwz@gmail.com>
2021-03-29 11:55:15 -0400

    add quoter

---
+++ contracts/examples/interfaces/IQuoter.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.7.5;
+pragma abicoder v2;
+
+/// @title Quoter
+interface IQuoter {
+    function exactInput(bytes memory path, uint256 amountIn) external returns (uint256 amountOut);
+    function exactOutput(bytes memory path, uint256 amountOut) external returns (uint256 amountIn);
+}
