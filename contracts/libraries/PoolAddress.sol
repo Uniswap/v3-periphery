54b6ddb4d0b73214b96300ecc3e6f059efa1b3f2
Moody Salem <moody.salem@gmail.com>
2021-03-31 13:38:00 -0500

    bump core version

--- contracts/libraries/PoolAddress.sol
+++ contracts/libraries/PoolAddress.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0;
 
 /// @dev Provides functions for deriving a pool address from the factory, tokens, and the fee
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0x01d4d358e07707f4db84b6a7527455b06f95ee89b5d059b4a1298ada7b6c7d67;
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0xa8180af292c6986c74fa300a542e049db8a89221e2452e431c3d8103b610c568;
 
     // The identifying key of the pool
     struct PoolKey {
