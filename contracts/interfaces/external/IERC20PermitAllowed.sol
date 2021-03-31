a4199f52e4fb604abb597ebfceba7e6204d99806
Sara Reynolds <snreynolds2506@gmail.com>
2021-03-31 12:27:02 -0700

    adding natspec for erc20 permit interface and erc1271 interface

--- contracts/interfaces/external/IERC20PermitAllowed.sol
+++ contracts/interfaces/external/IERC20PermitAllowed.sol
@@ -1,10 +1,19 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity >=0.5.0;
 
+/// @title Interface for permit
 /// @notice Interface used by DAI/CHAI for permit
 interface IERC20PermitAllowed {
     /// @notice Approve the spender to spend some tokens via the holder signature
     /// @dev This is the permit interface used by DAI and CHAI
+    /// @param holder the address of the token holder, the token owner
+    /// @param spender the address of the token spender
+    /// @param nonce the holder's nonce, increases at each call to permit
+    /// @param expiry the timestamp at which the permit is no longer valid
+    /// @param allowed boolean that sets approval, either 0 or unit(-1)
+    /// @param v must produce valid secp256k1 signature from the holder along with `r` and `s`
+    /// @param r must produce valid secp256k1 signature from the holder along with `v` and `s`
+    /// @param s must produce valid secp256k1 signature from the holder along with `r` and `v`
     function permit(
         address holder,
         address spender,
