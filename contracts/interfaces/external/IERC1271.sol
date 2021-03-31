ff6947a63fbb41fd3d0e6db97ddf0fa1967c7b7d
Sara Reynolds <snreynolds2506@gmail.com>
2021-03-31 13:50:34 -0700

    fixing capitalization

--- contracts/interfaces/external/IERC1271.sol
+++ contracts/interfaces/external/IERC1271.sol
@@ -11,6 +11,6 @@ interface IERC1271 {
     /// MUST allow external calls.
     /// @param hash Hash of the data to be signed
     /// @param signature Signature byte array associated with _data
-    /// @return magicValue the bytes4 magic value 0x1626ba7e
+    /// @return magicValue The bytes4 magic value 0x1626ba7e
     function isValidSignature(bytes32 hash, bytes memory signature) external view returns (bytes4 magicValue);
 }
--- contracts/interfaces/external/IERC20PermitAllowed.sol
+++ contracts/interfaces/external/IERC20PermitAllowed.sol
@@ -6,14 +6,14 @@ pragma solidity >=0.5.0;
 interface IERC20PermitAllowed {
     /// @notice Approve the spender to spend some tokens via the holder signature
     /// @dev This is the permit interface used by DAI and CHAI
-    /// @param holder the address of the token holder, the token owner
-    /// @param spender the address of the token spender
-    /// @param nonce the holder's nonce, increases at each call to permit
-    /// @param expiry the timestamp at which the permit is no longer valid
-    /// @param allowed boolean that sets approval, either 0 or unit(-1)
-    /// @param v must produce valid secp256k1 signature from the holder along with `r` and `s`
-    /// @param r must produce valid secp256k1 signature from the holder along with `v` and `s`
-    /// @param s must produce valid secp256k1 signature from the holder along with `r` and `v`
+    /// @param holder The address of the token holder, the token owner
+    /// @param spender The address of the token spender
+    /// @param nonce The holder's nonce, increases at each call to permit
+    /// @param expiry The timestamp at which the permit is no longer valid
+    /// @param allowed Boolean that sets approval, either 0 or unit(-1)
+    /// @param v Must produce valid secp256k1 signature from the holder along with `r` and `s`
+    /// @param r Must produce valid secp256k1 signature from the holder along with `v` and `s`
+    /// @param s Must produce valid secp256k1 signature from the holder along with `r` and `v`
     function permit(
         address holder,
         address spender,
