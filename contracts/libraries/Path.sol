159dbe129b91f263408cbff94fbb56631f166951
Connor Martin <cnr.mrtn@gmail.com>
2021-04-01 10:10:30 -0400

    path, pooladdress, transferhelper

--- contracts/libraries/Path.sol
+++ contracts/libraries/Path.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.6.0;
 
 import './BytesLib.sol';
 
+/// @title Functions for manipulating path data for multihop swaps
 library Path {
     using BytesLib for bytes;
 
@@ -12,12 +13,18 @@ library Path {
     uint256 private constant NEXT_OFFSET = ADDR_SIZE + FEE_SIZE;
     uint256 private constant POP_OFFSET = NEXT_OFFSET + ADDR_SIZE;
 
-    // checks if the path contains more than one pool
+    /// @notice Checks if the path contains more than one pool
+    /// @param path The swap path
+    /// @return True for more than one pool, false for one pool.
     function hasPools(bytes memory path) internal pure returns (bool) {
         return path.length > POP_OFFSET;
     }
 
-    // decodes the first pool in path
+    /// @notice Decodes the first pool in path
+    /// @param path The swap path
+    /// @return tokenA The first token of the given pool
+    /// @return tokenB The second token of the given pool
+    /// @return fee The fee level of the pool
     function decodeFirstPool(bytes memory path)
         internal
         pure
@@ -32,12 +39,16 @@ library Path {
         tokenB = path.toAddress(NEXT_OFFSET);
     }
 
-    // gets the segment corresponding to the first pool in the path
+    /// @notice Gets the segment corresponding to the first pool in the path
+    /// @param path The swap path
+    /// @return The segment containing all data necessary to target the first pool in the path
     function getFirstPool(bytes memory path) internal pure returns (bytes memory) {
         return path.slice(0, POP_OFFSET);
     }
 
-    // skips a token + fee element from the buffer and returns the remainder
+    /// @notice Skips a token + fee element from the buffer and returns the remainder
+    /// @param path The swap path
+    /// @return The remaining token + fee elements in the path
     function skipToken(bytes memory path) internal pure returns (bytes memory) {
         return path.slice(NEXT_OFFSET, path.length - NEXT_OFFSET);
     }
