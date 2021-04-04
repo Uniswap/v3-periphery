a8a645d3c95c0a355af615494f49fec021a4dd11
Emily Williams <emag3m@gmail.com>
2021-04-03 21:36:36 -0400

    Import upgradeability contracts

--- contracts/test/TestUpgradeability.sol
+++ contracts/test/TestUpgradeability.sol
@@ -3,5 +3,3 @@ pragma solidity =0.7.6;
 
 import '@openzeppelin/contracts/proxy/TransparentUpgradeableProxy.sol';
 import '@openzeppelin/contracts/proxy/ProxyAdmin.sol';
-
-abstract contract TestProxy is TransparentUpgradeableProxy {}
