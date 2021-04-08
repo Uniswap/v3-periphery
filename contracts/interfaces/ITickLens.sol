596fea833f572cfb26156b9c1388cbd524ef9210
Sara Reynolds <30504811+snreynolds@users.noreply.github.com>
2021-04-08 07:40:36 -0700

    SelfPermit, TickLens, and V3Migrator documentation (#82)
    * SelfPermit, TickLens, and V3Migrator documentation
    
    * Fix code style issues with Prettier
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- contracts/interfaces/ITickLens.sol
+++ contracts/interfaces/ITickLens.sol
@@ -17,7 +17,7 @@ interface ITickLens {
     /// @param pool The address of the pool for which to fetch populated tick data
     /// @param tickBitmapIndex The index of the word in the tick bitmap for which to parse the bitmap and
     /// fetch all the populated ticks
-    /// @return populatedTicks an array of tick data for the given word in the tick bitmap
+    /// @return populatedTicks An array of tick data for the given word in the tick bitmap
     function getPopulatedTicksInWord(address pool, int16 tickBitmapIndex)
         external
         view
--- contracts/interfaces/IV3Migrator.sol
+++ contracts/interfaces/IV3Migrator.sol
@@ -8,6 +8,13 @@ import './ISelfPermit.sol';
 /// @title V3 Migrator
 /// @notice Enables migration of liqudity from Uniswap v2-compatible pairs into Uniswap v3 pools
 interface IV3Migrator is IMulticall, ISelfPermit {
+    /// @notice Creates a new pool if it does not exist, then initializes if not initialized
+    /// @dev This method can wrapped for use in multicall
+    /// @param tokenA The contract address of either token0 or token1 in the v2 pair
+    /// We use tokenA and tokenB when we are referring to unsorted, or unordered tokens
+    /// @param tokenB The contract address of the other token, unsorted
+    /// @param fee The fee amount of the v3 pool for the specified token pair
+    /// @param sqrtPriceX96 The initial square root price of the pool as a Q64.96 value
     function createAndInitializePoolIfNecessary(
         address tokenA,
         address tokenB,
@@ -29,5 +36,8 @@ interface IV3Migrator is IMulticall, ISelfPermit {
         bool refundAsETH;
     }
 
+    /// @notice Migrates liquidity to v3 by burning v2 liquidity and minting a new position for v3
+    /// @dev Throws require exception if the migrated liquidity is less than `params.liquidityV3Min`
+    /// @param params The params necessary to migrate v2 liquidity, encoded as `MigrateParams` in calldata
     function migrate(MigrateParams calldata params) external;
 }
