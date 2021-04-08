f2a7c524f4d25bc43efff4c972d9c1a35a4f4533
Sara Reynolds <snreynolds2506@gmail.com>
2021-04-08 12:30:55 -0700

    IQuoter, IPeripheryPayments, IPeripheryImmutableState natspec

--- contracts/interfaces/IPeripheryImmutableState.sol
+++ contracts/interfaces/IPeripheryImmutableState.sol
@@ -4,9 +4,9 @@ pragma solidity >=0.5.0;
 /// @title Immutable state
 /// @notice Functions that return immutable state of the router
 interface IPeripheryImmutableState {
-    /// @notice Returns the address of the Uniswap V3 factory
+    /// @return Returns the address of the Uniswap V3 factory
     function factory() external view returns (address);
 
-    /// @notice Returns the address of WETH9
+    /// @return Returns the address of WETH9
     function WETH9() external view returns (address);
 }
