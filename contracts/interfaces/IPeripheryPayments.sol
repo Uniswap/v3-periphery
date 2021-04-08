06afe38f6f595941475bfb43ee2c77caca5253c1
Sara Reynolds <30504811+snreynolds@users.noreply.github.com>
2021-04-08 13:49:53 -0700

    IQuoter, IPeripheryPayments, IPeripheryImmutableState natspec (#84)

--- contracts/interfaces/IPeripheryPayments.sol
+++ contracts/interfaces/IPeripheryPayments.sol
@@ -6,10 +6,15 @@ pragma solidity >=0.7.5;
 interface IPeripheryPayments {
     /// @notice Unwraps the contract's WETH9 balance and sends it to recipient as ETH.
     /// @dev The amountMinimum parameter prevents malicious contracts from stealing WETH9 from users.
+    /// @param amountMinimum The minimum amount of WETH9 to unwrap
+    /// @param recipient The address receiving ETH
     function unwrapWETH9(uint256 amountMinimum, address recipient) external payable;
 
     /// @notice Sends the full amount of a token held by this contract to the given recipient
     /// @dev The amountMinimum parameter prevents malicious contracts from stealing the token from users
+    /// @param token The contract address of the token which will be transferred to `recipient`
+    /// @param amountMinimum The minimum amount of token required for a transfer
+    /// @param recipient The destination address of the token
     function sweepToken(
         address token,
         uint256 amountMinimum,
