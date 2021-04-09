9ca9575d09b0b8d985cc4d9a0f689f7a4470ecb7
Sara Reynolds <30504811+snreynolds@users.noreply.github.com>
2021-04-09 07:17:57 -0700

    natspec for INonfungiblePositionManager and INonfungibleTokenPositionDescriptor (#85)
    * IQuoter, IPeripheryPayments, IPeripheryImmutableState natspec
    
    * INonfungiblePositionManager and INonfungibleTokenPositionDescriptor natspec updates
    
    * spacing fix

--- contracts/interfaces/INonfungiblePositionManager.sol
+++ contracts/interfaces/INonfungiblePositionManager.sol
@@ -35,8 +35,20 @@ interface INonfungiblePositionManager is IPeripheryImmutableState, IERC721Metada
     event Collect(uint256 indexed tokenId, address recipient, uint256 amount0, uint256 amount1);
 
     /// @notice Returns the position information associated with a given token ID.
-    /// @param tokenId The ID of the token that represents the position
     /// @dev Throws if the token ID is not valid.
+    /// @param tokenId The ID of the token that represents the position
+    /// @return nonce The nonce for permits
+    /// @return operator The address that is approved for spending
+    /// @return token0 The address of the token0 for a specific pool
+    /// @return token1 The address of the token1 for a specific pool
+    /// @return fee The fee associated with the pool
+    /// @return tickLower The lower end of the tick range for the position
+    /// @return tickUpper The higher end of the tick range for the position
+    /// @return liquidity The liquidity of the position
+    /// @return feeGrowthInside0LastX128 The fee growth of token0 as of the last action on the individual position
+    /// @return feeGrowthInside1LastX128 The fee growth of token1 as of the last action on the individual position
+    /// @return tokensOwed0 The uncollected amount of token0 owed to the position as of the last computation
+    /// @return tokensOwed1 The uncollected amount of token1 owed to the position as of the last computation
     function positions(uint256 tokenId)
         external
         view
@@ -57,6 +69,12 @@ interface INonfungiblePositionManager is IPeripheryImmutableState, IERC721Metada
 
     /// @notice Creates a new pool if it does not exist, then initializes if not initialized
     /// @dev This method can be bundled with mint for the first mint of a pool to create, initialize a pool and mint at the same time
+    /// @param tokenA The contract address of either token0 or token1
+    /// We use tokenA and tokenB when we are referring to unsorted, or unordered tokens
+    /// @param tokenB The contract address of the other token, unsorted
+    /// @param fee The fee amount of the v3 pool for the specified token pair
+    /// @param sqrtPriceX96 The initial square root price of the pool as a Q64.96 value
+    /// @return pool Returns the pool address based on the pair of tokens and fee, will return the newly created pool address if necessary
     function createAndInitializePoolIfNecessary(
         address tokenA,
         address tokenB,
@@ -81,6 +99,11 @@ interface INonfungiblePositionManager is IPeripheryImmutableState, IERC721Metada
     /// @notice Creates a new position wrapped in a NFT
     /// @dev Call this when the pool does exist and is initialized. Note that if the pool is created but not initialized
     /// a method does not exist, i.e. the pool is assumed to be initialized.
+    /// @param params The params necessary to mint a position, encoded as `MintParams` in calldata
+    /// @return tokenId The ID of the token that represents the minted position
+    /// @return liquidity The amount of liquidity for this position
+    /// @return amount0 The amount of token0
+    /// @return amount1 The amount of token1
     function mint(MintParams calldata params)
         external
         payable
@@ -98,6 +121,9 @@ interface INonfungiblePositionManager is IPeripheryImmutableState, IERC721Metada
     /// @param amount0Min The minimum amount of token0 to spend, which serves as a slippage check
     /// @param amount1Min The minimum amount of token1 to spend, which serves as a slippage check
     /// @param deadline The time by which the transaction must be included to effect the change
+    /// @return liquidity The new liquidity amount as a result of the increase
+    /// @return amount0 The amount of token0 to acheive resulting liquidity
+    /// @return amount1 The amount of token1 to acheive resulting liquidity
     function increaseLiquidity(
         uint256 tokenId,
         uint256 amount0Desired,
@@ -119,6 +145,9 @@ interface INonfungiblePositionManager is IPeripheryImmutableState, IERC721Metada
     /// @param amount The amount by which liquidity will be decreased
     /// @param amount0Min The minimum amount of token0 that should be received in the burn
     /// @param amount1Min The minimum amount of token1 that should be received in the burn
+    /// @param deadline The time by which the transaction must be included to effect the change
+    /// @return amount0 The amount of token0 sent to recipient
+    /// @return amount1 The amount of token1 sent to recipient
     function decreaseLiquidity(
         uint256 tokenId,
         uint128 amount,
@@ -129,9 +158,11 @@ interface INonfungiblePositionManager is IPeripheryImmutableState, IERC721Metada
 
     /// @notice Collects up to a maximum amount of fees owed to a specific position to the recipient
     /// @param tokenId The ID of the NFT for which tokens are being collected
+    /// @param recipient The account that should receive the tokens
     /// @param amount0Max The maximum amount of token0 to collect
     /// @param amount1Max The maximum amount of token1 to collect
-    /// @param recipient The account that should receive the tokens
+    /// @return amount0 The amount of fees collected in token0
+    /// @return amount1 The amount of fees collected in token1
     function collect(
         uint256 tokenId,
         address recipient,
