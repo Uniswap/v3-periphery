e63c6ce344be053947ad27893702d442ba001cbc
Moody Salem <moody.salem@gmail.com>
2021-04-22 14:28:33 -0500

    improvements to payments - pay only uses partial ETH input amounts - add a method for refunding eth to the sender for mint/increase liquidity/exact output with eth input

--- contracts/interfaces/IPeripheryPayments.sol
+++ contracts/interfaces/IPeripheryPayments.sol
@@ -10,6 +10,11 @@ interface IPeripheryPayments {
     /// @param recipient The address receiving ETH
     function unwrapWETH9(uint256 amountMinimum, address recipient) external payable;
 
+    /// @notice Refunds any ETH balance held by this contract to the `msg.sender`
+    /// @dev Useful for bundling with mint or increase liquidity that uses ether, or exact output swaps
+    /// that use ether for the input amount
+    function refundETH() external payable;
+
     /// @notice Transfers the full amount of a token held by this contract to recipient
     /// @dev The amountMinimum parameter prevents malicious contracts from stealing the token from users
     /// @param token The contract address of the token which will be transferred to `recipient`
