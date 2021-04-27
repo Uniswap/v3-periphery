d13ded7685b1f4483f65b35e38befa6be10524f4
Moody Salem <moody.salem@gmail.com>
2021-04-26 20:56:22 -0500

    significantly cheaper encoding

--- contracts/libraries/Base64.sol
+++ contracts/libraries/Base64.sol
@@ -7,42 +7,76 @@ library Base64 {
     bytes32 internal constant TABLE0 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdef';
     bytes32 internal constant TABLE1 = 'ghijklmnopqrstuvwxyz0123456789+/';
 
+    byte internal constant FILLER = '=';
+
     /// @notice Encodes some bytes to the base64 representation
+    /// @dev Adapted from https://github.com/OpenZeppelin/solidity-jwt/blob/6ce4f3849005ded461b4c534c907b6d685feaac7/contracts/Base64.sol
     function encode(bytes memory data) internal pure returns (string memory) {
-        uint256 bitLen = data.length * 8;
-        if (bitLen == 0) return '';
+        uint256 rem = data.length % 3;
 
-        // multiply by 4/3 rounded up
-        uint256 encodedLen = (data.length * 4 + 2) / 3;
-        // result length must be a multiple of 4
-        if (encodedLen % 4 != 0) encodedLen += 4 - (encodedLen % 4);
+        uint256 encodedLength = (data.length * 4 + 2) / 3;
+        if (encodedLength % 4 != 0) encodedLength += 4 - (encodedLength % 4);
+        bytes memory result = new bytes(encodedLength);
 
-        bytes memory result = new bytes(encodedLen);
+        uint256 j = 0;
 
-        for (uint256 i = 0; i < encodedLen; i++) {
-            uint256 bitStartIndex = (i * 6);
-            if (bitStartIndex >= bitLen) {
-                result[i] = '=';
+        for (uint256 i = 0; i + 3 <= data.length; i += 3) {
+            (result[j], result[j + 1], result[j + 2], result[j + 3]) = encode3(
+                uint8(data[i]),
+                uint8(data[i + 1]),
+                uint8(data[i + 2])
+            );
+
+            j += 4;
+        }
+
+        if (rem != 0) {
+            uint8 la0 = uint8(data[data.length - rem]);
+            uint8 la1 = 0;
+
+            if (rem == 2) {
+                la1 = uint8(data[data.length - 1]);
+            }
+
+            (byte b0, byte b1, byte b2, ) = encode3(la0, la1, 0);
+            result[j] = b0;
+            result[j + 1] = b1;
+            if (rem == 2) {
+                result[j + 2] = b2;
+                result[j + 3] = FILLER;
             } else {
-                uint256 byteIndex = (bitStartIndex / 8);
-                uint8 bsiMod8 = uint8(bitStartIndex % 8);
-                if (bsiMod8 < 3) {
-                    uint8 c = (uint8(data[byteIndex]) >> (2 - bsiMod8)) % 64;
-                    if (c < 32) result[i] = TABLE0[c];
-                    else result[i] = TABLE1[c - 32];
-                } else {
-                    uint16 bytesCombined =
-                        (uint16(uint8(data[byteIndex])) << 8) +
-                            (byteIndex == data.length - 1 ? 0 : uint16(uint8(data[byteIndex + 1])));
-
-                    uint8 c = uint8((bytesCombined >> uint8(10 - bsiMod8)) % 64);
-
-                    if (c < 32) result[i] = TABLE0[c];
-                    else result[i] = TABLE1[c - 32];
-                }
+                result[j + 2] = FILLER;
+                result[j + 3] = FILLER;
             }
         }
 
         return string(result);
+    }
+
+    function encode3(
+        uint256 a0,
+        uint256 a1,
+        uint256 a2
+    )
+        private
+        pure
+        returns (
+            byte b0,
+            byte b1,
+            byte b2,
+            byte b3
+        )
+    {
+        uint256 n = (a0 << 16) | (a1 << 8) | a2;
+
+        uint256 c0 = (n >> 18) & 63;
+        uint256 c1 = (n >> 12) & 63;
+        uint256 c2 = (n >> 6) & 63;
+        uint256 c3 = (n) & 63;
+
+        b0 = c0 < 32 ? TABLE0[c0] : TABLE1[c0 - 32];
+        b1 = c1 < 32 ? TABLE0[c1] : TABLE1[c1 - 32];
+        b2 = c2 < 32 ? TABLE0[c2] : TABLE1[c2 - 32];
+        b3 = c3 < 32 ? TABLE0[c3] : TABLE1[c3 - 32];
     }
 }
