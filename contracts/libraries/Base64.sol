6d0cd0db1223d3142a14758315276bce24b91bf2
Moody Salem <moody.salem@gmail.com>
2021-04-26 21:52:23 -0500

    use the base64 encoding implementation from Brecht Devos

--- contracts/libraries/Base64.sol
+++ contracts/libraries/Base64.sol
@@ -1,82 +1,65 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0;
 
 /// @title Base64
 /// @notice Provides a function for encoding some bytes in base64
+/// @author Brecht Devos <brecht@loopring.org>
 library Base64 {
-    bytes32 internal constant TABLE0 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdef';
-    bytes32 internal constant TABLE1 = 'ghijklmnopqrstuvwxyz0123456789+/';
-
-    byte internal constant FILLER = '=';
+    bytes internal constant TABLE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
 
     /// @notice Encodes some bytes to the base64 representation
-    /// @dev Adapted from https://github.com/OpenZeppelin/solidity-jwt/blob/6ce4f3849005ded461b4c534c907b6d685feaac7/contracts/Base64.sol
     function encode(bytes memory data) internal pure returns (string memory) {
-        uint256 rem = data.length % 3;
+        if (data.length == 0) return '';
 
-        uint256 encodedLength = (data.length * 4 + 2) / 3;
-        if (encodedLength % 4 != 0) encodedLength += 4 - (encodedLength % 4);
-        bytes memory result = new bytes(encodedLength);
+        // multiply by 4/3 rounded up
+        uint256 encodedLen = 4 * ((data.length + 2) / 3);
 
-        uint256 j = 0;
+        // Add some extra buffer at the end
+        bytes memory result = new bytes(encodedLen + 32);
 
-        for (uint256 i = 0; i + 3 <= data.length; i += 3) {
-            (result[j], result[j + 1], result[j + 2], result[j + 3]) = encode3(
-                uint8(data[i]),
-                uint8(data[i + 1]),
-                uint8(data[i + 2])
-            );
-
-            j += 4;
+        bytes memory table = TABLE;
+        uint256 tablePtr;
+        assembly {
+            tablePtr := add(table, 1)
         }
 
-        if (rem != 0) {
-            uint8 la0 = uint8(data[data.length - rem]);
-            uint8 la1 = 0;
-
-            if (rem == 2) {
-                la1 = uint8(data[data.length - 1]);
+        uint256 outOffset = 32;
+        for (uint256 i = 0; i < data.length; ) {
+            i += 3;
+            uint256 input;
+            assembly {
+                input := and(mload(add(data, i)), 0xffffff)
             }
 
-            (byte b0, byte b1, byte b2, ) = encode3(la0, la1, 0);
-            result[j] = b0;
-            result[j + 1] = b1;
-            if (rem == 2) {
-                result[j + 2] = b2;
-                result[j + 3] = FILLER;
-            } else {
-                result[j + 2] = FILLER;
-                result[j + 3] = FILLER;
+            assembly {
+                let out := and(mload(add(tablePtr, and(shr(18, input), 0x3F))), 0xFF)
+                out := shl(8, out)
+                out := add(out, and(mload(add(tablePtr, and(shr(12, input), 0x3F))), 0xFF))
+                out := shl(8, out)
+                out := add(out, and(mload(add(tablePtr, and(shr(6, input), 0x3F))), 0xFF))
+                out := shl(8, out)
+                out := add(out, and(mload(add(tablePtr, and(input, 0x3F))), 0xFF))
+                out := shl(224, out)
+
+                mstore(add(result, outOffset), out)
             }
+            outOffset += 4;
+        }
+
+        // Padding
+        uint256 r = data.length % 3;
+        if (r != 0) {
+            r = (r == 1) ? 2 : 1;
+        }
+        for (uint256 i = 0; i < r; i++) {
+            result[encodedLen - 1 - i] = '=';
+        }
+
+        // Set the actual output length
+        assembly {
+            mstore(result, encodedLen)
         }
 
         return string(result);
-    }
-
-    function encode3(
-        uint256 a0,
-        uint256 a1,
-        uint256 a2
-    )
-        private
-        pure
-        returns (
-            byte b0,
-            byte b1,
-            byte b2,
-            byte b3
-        )
-    {
-        uint256 n = (a0 << 16) | (a1 << 8) | a2;
-
-        uint256 c0 = (n >> 18) & 63;
-        uint256 c1 = (n >> 12) & 63;
-        uint256 c2 = (n >> 6) & 63;
-        uint256 c3 = (n) & 63;
-
-        b0 = c0 < 32 ? TABLE0[c0] : TABLE1[c0 - 32];
-        b1 = c1 < 32 ? TABLE0[c1] : TABLE1[c1 - 32];
-        b2 = c2 < 32 ? TABLE0[c2] : TABLE1[c2 - 32];
-        b3 = c3 < 32 ? TABLE0[c3] : TABLE1[c3 - 32];
     }
 }
