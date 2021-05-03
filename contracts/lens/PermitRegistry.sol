cd331bcb4159e908b5cc7847365704ce812ed441
Moody Salem <moody.salem@gmail.com>
2021-05-03 10:51:31 -0500

    some refactoring

--- contracts/lens/PermitRegistry.sol
+++ contracts/lens/PermitRegistry.sol
@@ -1,82 +1,41 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity =0.7.6;
+pragma abicoder v2;
 
-pragma solidity >=0.7.0 <0.8.0-0||>=0.8.0 <0.9.0-0;
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/drafts/IERC20Permit.sol';
 
-interface IERC20 {
-    /// @dev Returns the remaining number of tokens that `spender` will be allowed to spend on behalf of `owner`.
-    function allowance(address owner, address spender) external view returns (uint256);
-}
+import '../interfaces/external/IERC1271.sol';
+import '../interfaces/external/IERC20PermitAllowed.sol';
+import '../base/Multicall.sol';
 
-interface IERC2612 {
-    /// @dev Sets `amount` as the allowance of `spender` over `owner`'s tokens, given `owner`'s signed approval.
-    function permit(
-        address owner,
-        address spender,
-        uint256 amount,
-        uint256 deadline,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external;
-
-    /// @dev Returns the current permit nonce for `owner`.
-    function nonces(address owner) external view returns (uint256);
-
-    /// @dev Return the ERC712 domain separator
-    function DOMAIN_SEPARATOR() external view returns (bytes32);
-}
-
-interface DaiAbstract {
-    // `nonces` and `DOMAIN_SEPARATOR()` are identical to IERC2612.
-
-    /// @dev Sets MAX as the allowance of `spender` over `owner`'s tokens, given `owner`'s signed approval.
-    function permit(
-        address,
-        address,
-        uint256,
-        uint256,
-        bool,
-        uint8,
-        bytes32,
-        bytes32
-    ) external;
-}
-
-interface IERC1271 {
-    /// @dev Should return whether the signature provided is valid for the provided data
-    /// @param hash      Hash of the data to be signed
-    /// @param signature Signature byte array associated with _data
-    function isValidSignature(bytes32 hash, bytes memory signature) external view returns (bytes4 magicValue);
-}
-
-contract PermitRegistry {
+contract PermitRegistry is Multicall, IERC1271 {
     event PermitRegistered(address indexed token, uint8 indexed permitType);
-    event DomainRegistered(address indexed token, string indexed name, string indexed version, bytes32 domain);
+    event DomainRegistered(address indexed token, string indexed name, string indexed version);
 
     uint8 public constant NONE = 0;
     uint8 public constant DAI = 1;
     uint8 public constant ERC2612 = 2;
     uint8 public constant ERC1271 = 4;
 
-    uint256 public constant MAX = type(uint256).max;
+    mapping(address => uint8) private _permits;
+    struct DomainComponents {
+        string name;
+        string version;
+    }
+    mapping(address => DomainComponents) private _domainComponents;
 
-    mapping(address => uint8) public permits;
-    mapping(address => string) public names;
-    mapping(address => string) public versions;
-    mapping(address => bytes32) public domains;
-
-    /// @dev Return all known data from a given address.
-    function data(address token)
-        public
+    /// @dev Return all registered data for a given token address.
+    function get(address token)
+        external
         view
         returns (
             uint8,
             string memory,
-            string memory,
-            bytes32
+            string memory
         )
     {
-        return (permits[token], names[token], versions[token], domains[token]);
+        return (_permits[token], _domainComponents[token].name, _domainComponents[token].version);
     }
 
     /// @dev Verify if the parameters were used to build a domain separator in a target contract, and store everything if positive.
@@ -101,11 +60,9 @@ contract PermitRegistry {
                 )
             );
 
-        require(domain == IERC2612(token).DOMAIN_SEPARATOR(), 'Mismatched domains');
-        names[token] = name;
-        versions[token] = version;
-        domains[token] = domain;
-        emit DomainRegistered(token, name, version, domain);
+        require(domain == IERC20Permit(token).DOMAIN_SEPARATOR(), 'Mismatched domains');
+        _domainComponents[token] = DomainComponents({name: name, version: version});
+        emit DomainRegistered(token, name, version);
     }
 
     /// @dev Use an ERC2612 permit to register if `token` implements it.
@@ -113,28 +70,28 @@ contract PermitRegistry {
     /// @notice As a precaution, set the allowance back to zero after registering.
     function registerPermit(
         address token,
+        address owner,
         uint8 v,
         bytes32 r,
         bytes32 s
     ) public {
-        IERC2612(token).permit(msg.sender, address(this), MAX, MAX, v, r, s);
-        require(IERC20(token).allowance(msg.sender, address(this)) == MAX, 'No ERC2612');
-        permits[token] &= ERC2612;
+        require(IERC20(token).allowance(owner, address(this)) == 0, 'Non-zero starting allowance');
+        IERC20Permit(token).permit(owner, address(this), 1, type(uint256).max, v, r, s);
+        require(IERC20(token).allowance(owner, address(this)) == 1, 'No ERC2612');
+        _permits[token] &= ERC2612;
         emit PermitRegistered(token, ERC2612);
     }
 
-    /// @dev Use an ERC1271 off-chain signature to register if `token` implements it.
-    function registerERC1271(
-        address token,
-        bytes32 hash,
-        bytes memory signature
-    ) public {
-        require(
-            IERC1271(token).isValidSignature(hash, signature) == IERC1271(token).isValidSignature.selector,
-            'No ERC1271'
-        );
-        permits[token] &= ERC1271;
+    /// @dev Check if permit can be approved
+    function registerERC1271(address token) public {
+        revert('TODO');
+        _permits[token] &= ERC1271;
         emit PermitRegistered(token, ERC1271);
+    }
+
+    function isValidSignature(bytes32 hash, bytes memory signature) external view override returns (bytes4) {
+        revert('todo');
+        return 0x1626ba7e;
     }
 
     /// @dev Use an Dai-style permit to register if `token` implements it.
@@ -142,14 +99,27 @@ contract PermitRegistry {
     /// @notice As a precaution, set the allowance back to zero after registering.
     function registerDaiPermit(
         address token,
+        address owner,
         uint8 v,
         bytes32 r,
         bytes32 s
     ) private {
-        uint256 nonce = IERC2612(token).nonces(msg.sender);
-        DaiAbstract(token).permit(msg.sender, address(this), nonce, MAX, true, v, r, s);
-        require(IERC20(token).allowance(msg.sender, address(this)) == MAX, 'No DAI permit');
-        permits[token] &= DAI;
+        require(IERC20(token).allowance(owner, address(this)) == 0, 'Non-zero starting allowance');
+        uint256 nonce = IERC20Permit(token).nonces(owner);
+        IERC20PermitAllowed(token).permit(
+            owner,
+            address(this),
+            nonce,
+            /* expiry= */
+            type(uint256).max,
+            /* allowed= */
+            true,
+            v,
+            r,
+            s
+        );
+        require(IERC20(token).allowance(owner, address(this)) == type(uint256).max, 'No DAI permit');
+        _permits[token] &= DAI;
         emit PermitRegistered(token, DAI);
     }
 }
