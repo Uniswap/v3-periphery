6a1891934cb060fe742c4aeb0335a4c4acb98d29
Lint Action <lint-action@samuelmeuli.com>
2021-05-06 21:09:18 +0000

    Fix code style issues with Prettier

--- contracts/examples/PairFlash.sol
+++ contracts/examples/PairFlash.sol
@@ -16,6 +16,7 @@ import '../libraries/TransferHelper.sol';
 
 import '../SwapRouter.sol';
 import '../interfaces/ISwapRouter.sol';
+
 /// @title Flash contract implementation
 /// @notice An example contract using the Uniswap V3 flash function
 contract PairFlash is IUniswapV3FlashCallback, PeripheryImmutableState, PeripheryPayments {
@@ -24,7 +25,11 @@ contract PairFlash is IUniswapV3FlashCallback, PeripheryImmutableState, Peripher
 
     ISwapRouter swapRouter;
 
-    constructor(ISwapRouter _swapRouter, address _factory, address _WETH9) PeripheryImmutableState(_factory, _WETH9) {
+    constructor(
+        ISwapRouter _swapRouter,
+        address _factory,
+        address _WETH9
+    ) PeripheryImmutableState(_factory, _WETH9) {
         swapRouter = _swapRouter;
     }
 
@@ -38,7 +43,6 @@ contract PairFlash is IUniswapV3FlashCallback, PeripheryImmutableState, Peripher
         uint256 fee1,
         bytes calldata data
     ) external override {
-
         FlashCallbackData memory decoded = abi.decode(data, (FlashCallbackData));
         CallbackValidation.verifyCallback(factory, decoded.poolKey);
 
@@ -48,53 +52,55 @@ contract PairFlash is IUniswapV3FlashCallback, PeripheryImmutableState, Peripher
         TransferHelper.safeApprove(token0, address(swapRouter), decoded.amount0);
         TransferHelper.safeApprove(token1, address(swapRouter), decoded.amount1);
 
-
-        // profitable check 
+        // profitable check
         // exactInputSingle will fail if this amount not met
         uint256 amount1Min = LowGasSafeMath.add(decoded.amount1, fee1);
         uint256 amount0Min = LowGasSafeMath.add(decoded.amount0, fee0);
 
         // call exactInputSingle for swapping token1 for token0 in pool w/fee1
-        uint256 amountOut0 = swapRouter.exactInputSingle(
-            ISwapRouter.ExactInputSingleParams(
-                {tokenIn: token1, 
-                tokenOut: token0, 
-                fee: decoded.poolFee2, 
-                recipient: address(this), 
-                deadline: block.timestamp + 200, 
-                amountIn: decoded.amount1, 
-                amountOutMinimum: amount0Min, 
-                sqrtPriceLimitX96: 0})
+        uint256 amountOut0 =
+            swapRouter.exactInputSingle(
+                ISwapRouter.ExactInputSingleParams({
+                    tokenIn: token1,
+                    tokenOut: token0,
+                    fee: decoded.poolFee2,
+                    recipient: address(this),
+                    deadline: block.timestamp + 200,
+                    amountIn: decoded.amount1,
+                    amountOutMinimum: amount0Min,
+                    sqrtPriceLimitX96: 0
+                })
             );
 
         // call exactInputSingle for swapping token0 for token 1 in pool w/fee2
-        uint256 amountOut1 = swapRouter.exactInputSingle(
-            ISwapRouter.ExactInputSingleParams(
-                {tokenIn: token0, 
-                tokenOut: token1, 
-                fee: decoded.poolFee3, 
-                recipient: address(this), 
-                deadline: block.timestamp + 200, 
-                amountIn: decoded.amount0, 
-                amountOutMinimum: amount1Min, 
-                sqrtPriceLimitX96: 0 }));
-     
+        uint256 amountOut1 =
+            swapRouter.exactInputSingle(
+                ISwapRouter.ExactInputSingleParams({
+                    tokenIn: token0,
+                    tokenOut: token1,
+                    fee: decoded.poolFee3,
+                    recipient: address(this),
+                    deadline: block.timestamp + 200,
+                    amountIn: decoded.amount0,
+                    amountOutMinimum: amount1Min,
+                    sqrtPriceLimitX96: 0
+                })
+            );
 
         // end up with amountOut0 of token0 from first swap and amountOut1 of token1 from second swap
         uint256 amount0Owed = LowGasSafeMath.add(decoded.amount0, fee0);
         uint256 amount1Owed = LowGasSafeMath.add(decoded.amount1, fee1);
-
 
         TransferHelper.safeApprove(token0, address(this), amount0Owed);
         TransferHelper.safeApprove(token1, address(this), amount1Owed);
 
         if (amount0Owed > 0) pay(token0, address(this), msg.sender, amount0Owed);
         if (amount1Owed > 0) pay(token1, address(this), msg.sender, amount1Owed);
-        
+
         // if profitable pay profits to payer
         if (amountOut0 > amount0Owed) {
             uint256 profit0 = LowGasSafeMath.sub(amountOut0, amount0Owed);
-            
+
             TransferHelper.safeApprove(token0, address(this), profit0);
             pay(token0, address(this), decoded.payer, profit0);
         }
@@ -105,10 +111,10 @@ contract PairFlash is IUniswapV3FlashCallback, PeripheryImmutableState, Peripher
         }
     }
 
-        //fee1 is the fee of the pool from the initial borrow 
-        //fee2 is the fee of the first pool to arb from
-        //fee3 is the fee of the second pool to arb from
-        struct FlashParams {
+    //fee1 is the fee of the pool from the initial borrow
+    //fee2 is the fee of the first pool to arb from
+    //fee3 is the fee of the second pool to arb from
+    struct FlashParams {
         address token0;
         address token1;
         uint24 fee1;
@@ -117,8 +123,8 @@ contract PairFlash is IUniswapV3FlashCallback, PeripheryImmutableState, Peripher
         uint24 fee2;
         uint24 fee3;
     }
-        // fee2 and fee3 are the two other fees associated with the two other pools of token0 and token1
-        struct FlashCallbackData {
+    // fee2 and fee3 are the two other fees associated with the two other pools of token0 and token1
+    struct FlashCallbackData {
         uint256 amount0;
         uint256 amount1;
         address payer;
@@ -131,7 +137,8 @@ contract PairFlash is IUniswapV3FlashCallback, PeripheryImmutableState, Peripher
     /// @notice Calls the pools flash function
     /// @dev passes in data needed for the callback, `uniswapV3FlashCallback`
     function initFlash(FlashParams memory params) external {
-        PoolAddress.PoolKey memory poolKey = PoolAddress.PoolKey({token0: params.token0, token1: params.token1, fee: params.fee1});
+        PoolAddress.PoolKey memory poolKey =
+            PoolAddress.PoolKey({token0: params.token0, token1: params.token1, fee: params.fee1});
         IUniswapV3Pool pool = IUniswapV3Pool(PoolAddress.computeAddress(factory, poolKey));
         // recipient of borrowed amounts
         // amount of token0 requested to borrow
@@ -142,7 +149,16 @@ contract PairFlash is IUniswapV3FlashCallback, PeripheryImmutableState, Peripher
             address(this),
             params.amount0,
             params.amount1,
-            abi.encode(FlashCallbackData({amount0: params.amount0, amount1: params.amount1, payer: msg.sender, poolKey: poolKey, poolFee2: params.fee2, poolFee3: params.fee3}))
+            abi.encode(
+                FlashCallbackData({
+                    amount0: params.amount0,
+                    amount1: params.amount1,
+                    payer: msg.sender,
+                    poolKey: poolKey,
+                    poolFee2: params.fee2,
+                    poolFee3: params.fee3
+                })
+            )
         );
     }
 }
--- test/PairFlash.spec.ts
+++ test/PairFlash.spec.ts
@@ -1,6 +1,18 @@
-import { ethers,waffle } from 'hardhat'
+import { ethers, waffle } from 'hardhat'
 import { BigNumber, constants, Contract, ContractTransaction } from 'ethers'
-import { IWETH9, MockTimeNonfungiblePositionManager, MockTimeSwapRouter, PairFlash, IUniswapV3Pool, TestERC20, TestERC20Metadata, IUniswapV3Factory, NFTDescriptor, Quoter, SwapRouter} from '../typechain'
+import {
+  IWETH9,
+  MockTimeNonfungiblePositionManager,
+  MockTimeSwapRouter,
+  PairFlash,
+  IUniswapV3Pool,
+  TestERC20,
+  TestERC20Metadata,
+  IUniswapV3Factory,
+  NFTDescriptor,
+  Quoter,
+  SwapRouter,
+} from '../typechain'
 import completeFixture from './shared/completeFixture'
 import { FeeAmount, MaxUint128, TICK_SPACINGS } from './shared/constants'
 import { encodePriceSqrt } from './shared/encodePriceSqrt'
@@ -11,27 +23,22 @@ import { getMaxTick, getMinTick } from './shared/ticks'
 import { computePoolAddress } from './shared/computePoolAddress'
 
 describe('PairFlash test', () => {
-    const provider = waffle.provider
-    const wallets = waffle.provider.getWallets()
-    const wallet = wallets[0]
-    
-    let flash: PairFlash
-    let nft: MockTimeNonfungiblePositionManager
-    let token0: TestERC20
-    let token1: TestERC20
-    let factory: IUniswapV3Factory
-    let quoter: Quoter
+  const provider = waffle.provider
+  const wallets = waffle.provider.getWallets()
+  const wallet = wallets[0]
 
-  async function createPool(tokenAddressA: string, tokenAddressB: string, fee: FeeAmount, price: BigNumber ) {
+  let flash: PairFlash
+  let nft: MockTimeNonfungiblePositionManager
+  let token0: TestERC20
+  let token1: TestERC20
+  let factory: IUniswapV3Factory
+  let quoter: Quoter
+
+  async function createPool(tokenAddressA: string, tokenAddressB: string, fee: FeeAmount, price: BigNumber) {
     if (tokenAddressA.toLowerCase() > tokenAddressB.toLowerCase())
       [tokenAddressA, tokenAddressB] = [tokenAddressB, tokenAddressA]
 
-    await nft.createAndInitializePoolIfNecessary(
-      tokenAddressA,
-      tokenAddressB,
-      fee,
-      price,
-    )
+    await nft.createAndInitializePoolIfNecessary(tokenAddressA, tokenAddressB, fee, price)
 
     const liquidityParams = {
       token0: tokenAddressA,
@@ -50,15 +57,13 @@ describe('PairFlash test', () => {
     return nft.mint(liquidityParams)
   }
 
-  const flashFixture = async() => {
-
-    
-    const {router, tokens, factory, weth9, nft} = await completeFixture(wallets, provider)
+  const flashFixture = async () => {
+    const { router, tokens, factory, weth9, nft } = await completeFixture(wallets, provider)
     const token0 = tokens[0]
     const token1 = tokens[1]
-    
+
     const flashContractFactory = await ethers.getContractFactory('PairFlash')
-    const flash = await flashContractFactory.deploy(router.address, factory.address, weth9.address ) as PairFlash
+    const flash = (await flashContractFactory.deploy(router.address, factory.address, weth9.address)) as PairFlash
 
     const quoterFactory = await ethers.getContractFactory('Quoter')
     const quoter = (await quoterFactory.deploy(factory.address, weth9.address)) as Quoter
@@ -82,72 +87,85 @@ describe('PairFlash test', () => {
   })
 
   beforeEach('load fixture', async () => {
-    ;({ factory, token0, token1, flash, nft, quoter} = await loadFixture(flashFixture))
-    
+    ;({ factory, token0, token1, flash, nft, quoter } = await loadFixture(flashFixture))
+
     await token0.approve(nft.address, MaxUint128)
     await token1.approve(nft.address, MaxUint128)
-    await createPool(token0.address, token1.address, FeeAmount.LOW, encodePriceSqrt(5,10))
-    await createPool(token0.address, token1.address, FeeAmount.MEDIUM, encodePriceSqrt(1,1))
-    await createPool(token0.address, token1.address, FeeAmount.HIGH, encodePriceSqrt(20,10))
-    // set up funds in flash contract 
+    await createPool(token0.address, token1.address, FeeAmount.LOW, encodePriceSqrt(5, 10))
+    await createPool(token0.address, token1.address, FeeAmount.MEDIUM, encodePriceSqrt(1, 1))
+    await createPool(token0.address, token1.address, FeeAmount.HIGH, encodePriceSqrt(20, 10))
+    // set up funds in flash contract
     await token0.transfer(flash.address, MaxUint128)
     await token1.transfer(flash.address, MaxUint128)
   })
 
-    describe.only('flash', () => {
-      
-        it('test correct transfer events', async () => {
-            //choose amountIn to test
-            const amount0In = 1000
-            const amount1In = 1000
+  describe.only('flash', () => {
+    it('test correct transfer events', async () => {
+      //choose amountIn to test
+      const amount0In = 1000
+      const amount1In = 1000
 
-            const fee0 = Math.ceil((amount0In*FeeAmount.MEDIUM)/1000000)// how can i get this ?
-            const fee1 = Math.ceil((amount1In*FeeAmount.MEDIUM)/1000000) // how can i get this dynamically?
+      const fee0 = Math.ceil((amount0In * FeeAmount.MEDIUM) / 1000000) // how can i get this ?
+      const fee1 = Math.ceil((amount1In * FeeAmount.MEDIUM) / 1000000) // how can i get this dynamically?
 
-            const flashParams = {
-                token0: token0.address,
-                token1: token1.address,
-                fee1: FeeAmount.MEDIUM,
-                amount0: amount0In,
-                amount1: amount1In,
-                fee2: FeeAmount.LOW,
-                fee3: FeeAmount.HIGH,
-            }
-            // pool1 is the borrow pool
-            const pool1 = computePoolAddress(factory.address, [token0.address, token1.address], FeeAmount.MEDIUM)
-            const pool2  = computePoolAddress(factory.address, [token0.address, token1.address], FeeAmount.LOW)
-            const pool3  = computePoolAddress(factory.address, [token0.address, token1.address], FeeAmount.HIGH)
-            
+      const flashParams = {
+        token0: token0.address,
+        token1: token1.address,
+        fee1: FeeAmount.MEDIUM,
+        amount0: amount0In,
+        amount1: amount1In,
+        fee2: FeeAmount.LOW,
+        fee3: FeeAmount.HIGH,
+      }
+      // pool1 is the borrow pool
+      const pool1 = computePoolAddress(factory.address, [token0.address, token1.address], FeeAmount.MEDIUM)
+      const pool2 = computePoolAddress(factory.address, [token0.address, token1.address], FeeAmount.LOW)
+      const pool3 = computePoolAddress(factory.address, [token0.address, token1.address], FeeAmount.HIGH)
 
-            const expectedAmountOut0 = await quoter.callStatic.quoteExactInputSingle(token1.address, token0.address, FeeAmount.LOW, amount1In, encodePriceSqrt(20,10))
-            const expectedAmountOut1 = await quoter.callStatic.quoteExactInputSingle(token0.address, token1.address, FeeAmount.HIGH, amount0In, encodePriceSqrt(5,10))
-            
-          await expect(
-            flash.initFlash(flashParams)
-          ).to.emit(token0, 'Transfer').withArgs(pool1, flash.address, amount0In).
-          to.emit(token1, 'Transfer').withArgs(pool1, flash.address,amount1In).
-          to.emit(token0, 'Transfer').withArgs(pool2, flash.address, expectedAmountOut0).
-          to.emit(token1, 'Transfer').withArgs(pool3, flash.address, expectedAmountOut1).
-          to.emit(token0, 'Transfer').withArgs(flash.address, wallet.address, expectedAmountOut0.toNumber() - amount0In - fee0).
-          to.emit(token1, 'Transfer').withArgs(flash.address, wallet.address, expectedAmountOut1.toNumber() - amount1In - fee1)
-        })
+      const expectedAmountOut0 = await quoter.callStatic.quoteExactInputSingle(
+        token1.address,
+        token0.address,
+        FeeAmount.LOW,
+        amount1In,
+        encodePriceSqrt(20, 10)
+      )
+      const expectedAmountOut1 = await quoter.callStatic.quoteExactInputSingle(
+        token0.address,
+        token1.address,
+        FeeAmount.HIGH,
+        amount0In,
+        encodePriceSqrt(5, 10)
+      )
 
-        it('gas', async () => {
-            const amount0In = 1000
-            const amount1In = 1000
-
-            const flashParams = {
-                token0: token0.address,
-                token1: token1.address,
-                fee1: FeeAmount.MEDIUM,
-                amount0: amount0In,
-                amount1: amount1In,
-                fee2: FeeAmount.LOW,
-                fee3: FeeAmount.HIGH,
-            }
-            await snapshotGasCost(
-              flash.initFlash(flashParams)
-            )
-          })
-        })
+      await expect(flash.initFlash(flashParams))
+        .to.emit(token0, 'Transfer')
+        .withArgs(pool1, flash.address, amount0In)
+        .to.emit(token1, 'Transfer')
+        .withArgs(pool1, flash.address, amount1In)
+        .to.emit(token0, 'Transfer')
+        .withArgs(pool2, flash.address, expectedAmountOut0)
+        .to.emit(token1, 'Transfer')
+        .withArgs(pool3, flash.address, expectedAmountOut1)
+        .to.emit(token0, 'Transfer')
+        .withArgs(flash.address, wallet.address, expectedAmountOut0.toNumber() - amount0In - fee0)
+        .to.emit(token1, 'Transfer')
+        .withArgs(flash.address, wallet.address, expectedAmountOut1.toNumber() - amount1In - fee1)
     })
+
+    it('gas', async () => {
+      const amount0In = 1000
+      const amount1In = 1000
+
+      const flashParams = {
+        token0: token0.address,
+        token1: token1.address,
+        fee1: FeeAmount.MEDIUM,
+        amount0: amount0In,
+        amount1: amount1In,
+        fee2: FeeAmount.LOW,
+        fee3: FeeAmount.HIGH,
+      }
+      await snapshotGasCost(flash.initFlash(flashParams))
+    })
+  })
+})
