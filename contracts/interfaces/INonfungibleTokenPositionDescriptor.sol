523e6a6108800911989dbb685cd19e1e2108966a
Emily Williams <emag3m@gmail.com>
2021-05-16 16:53:53 -0400

    take out never emitted event

--- contracts/interfaces/INonfungibleTokenPositionDescriptor.sol
+++ contracts/interfaces/INonfungibleTokenPositionDescriptor.sol
@@ -5,11 +5,6 @@ import './INonfungiblePositionManager.sol';
 
 /// @title Describes position NFT tokens via URI
 interface INonfungibleTokenPositionDescriptor {
-    /// @notice Emitted when a token is given a new priority order in the displayed price ratio
-    /// @param token The token being given priority order
-    /// @param priority Represents priority in ratio - higher integers get numerator priority
-    event UpdateTokenRatioPriority(address token, int256 priority);
-
     /// @notice Produces the URI describing a particular token ID for a position manager
     /// @dev Note this URI may be a data: URI with the JSON contents directly inlined
     /// @param positionManager The position manager for which to describe the token
