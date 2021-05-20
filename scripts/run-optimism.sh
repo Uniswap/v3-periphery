c9857f201f52b2e4a3c911b101d0d313675ed258
elenadimitrova <elena@arenabg.com>
2021-05-20 18:12:47 +0300

    Add scripts for starting/stopping the optimism services (copy from core)

---
+++ scripts/run-optimism.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# build in 2 steps
+function build_images() {
+  docker-compose build --parallel -- builder l2geth l1_chain
+  docker-compose build --parallel -- deployer dtl batch_submitter relayer
+}
+
+current_dir=$(dirname "$0")
+
+git clone https://github.com/ethereum-optimism/optimism.git
+cd optimism/ops
+# TODO: Remove once the v0.4.0 regenesis is merged.
+git checkout fix/11m-gas-limit
+
+# enabling the Docker BuildKit is recommended
+export COMPOSE_DOCKER_CLI_BUILD=1
+export DOCKER_BUILDKIT=1
+
+build_images
+docker-compose up -d
+../../${current_dir}/wait-for-sequencer.sh
