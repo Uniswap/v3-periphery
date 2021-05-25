b6b57c758338354cac1d719e0944a8b0921cd562
Matt Solomon <matt@mattsolomon.dev>
2021-05-24 19:19:44 -0700

    Fix: when testing on OVM, use pre-deployed WETH instead of deploying it

--- test/shared/externalFixtures.ts
+++ test/shared/externalFixtures.ts
@@ -1,6 +1,6 @@
 import { abi as FACTORY_V2_ABI, bytecode as FACTORY_V2_BYTECODE } from '@uniswap/v2-core/build/UniswapV2Factory.json'
 import { Fixture } from 'ethereum-waffle'
-import { ethers, waffle } from 'hardhat'
+import { ethers, network, waffle } from 'hardhat'
 import { IUniswapV3Factory, IWETH9, MockTimeSwapRouter } from '../../typechain'
 
 import WETH9 from '../contracts/WETH9.json'
@@ -9,6 +9,12 @@ import { constants } from 'ethers'
 import { v3CoreFactoryFixtureSetup } from './setup'
 
 const wethFixture: Fixture<{ weth9: IWETH9 }> = async ([wallet]) => {
+  // If testing on OVM, get the pre-deployed WETH contract
+  if (network.name === 'optimism') {
+    const weth9 = (await ethers.getContractAt(WETH9.abi, '0x4200000000000000000000000000000000000006')) as IWETH9
+    return { weth9 }
+  }
+  // Otherwise deploy WETH9
   const weth9 = (await waffle.deployContract(wallet, {
     bytecode: WETH9.bytecode,
     abi: WETH9.abi,
