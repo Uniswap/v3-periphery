26519a2685bacaf5dd754f8a1cbf5387631ad578
Matt Solomon <matt@mattsolomon.dev>
2021-05-27 13:52:26 -0700

    Restore isContract check so EVM tests pass (undo 42577c079324348be75a9251b031d3ad7812898d)

--- contracts/base/ERC721Permit.sol
+++ contracts/base/ERC721Permit.sol
@@ -72,7 +72,13 @@ abstract contract ERC721Permit is BlockTimestamp, ERC721, IERC721Permit {
         address owner = ownerOf(tokenId);
         require(spender != owner, 'ERC721Permit: approval to current owner');
 
-        require(IERC1271(owner).isValidSignature(digest, abi.encodePacked(r, s, v)) == 0x1626ba7e, 'Unauthorized');
+        if (Address.isContract(owner)) {
+            require(IERC1271(owner).isValidSignature(digest, abi.encodePacked(r, s, v)) == 0x1626ba7e, 'Unauthorized');
+        } else {
+            address recoveredAddress = ecrecover(digest, v, r, s);
+            require(recoveredAddress != address(0), 'Invalid signature');
+            require(recoveredAddress == owner, 'Unauthorized');
+        }
 
         _approve(spender, tokenId);
     }
