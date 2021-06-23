32e290bbdc1442c13587705cb901eb94787407a6
Emily Williams <emag3m@gmail.com>
2021-06-23 16:59:29 -0600

    pass at fees

--- contracts/libraries/PositionValue.sol
+++ contracts/libraries/PositionValue.sol
@@ -2,10 +2,12 @@
 pragma solidity >=0.7.0;
 
 import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
+import '@uniswap/v3-core/contracts/libraries/FixedPoint128.sol';
 import '@uniswap/v3-core/contracts/libraries/TickMath.sol';
 import '../interfaces/INonfungiblePositionManager.sol';
 import './LiquidityAmounts.sol';
 import './PoolAddress.sol';
+import './PositionKey.sol';
 
 library PositionValue {
     function principal(INonfungiblePositionManager nft, uint256 tokenId)
@@ -32,5 +34,50 @@ library PositionValue {
                 TickMath.getSqrtRatioAtTick(tickUpper),
                 liquidity
             );
+    }
+
+    function fees(INonfungiblePositionManager nft, uint256 tokenId)
+        internal
+        view
+        returns (uint256 amount0, uint256 amount1)
+    {
+        (
+            ,
+            ,
+            address token0,
+            address token1,
+            uint24 fee,
+            int24 tickLower,
+            int24 tickUpper,
+            uint128 liquidity,
+            uint256 positionFeeGrowthInside0LastX128,
+            uint256 positionFeeGrowthInside1LastX128,
+            uint128 tokensOwed0,
+            uint128 tokensOwed1
+        ) = nft.positions(tokenId);
+
+        (, uint256 poolFeeGrowthInside0LastX128, uint256 poolFeeGrowthInside1LastX128, , ) =
+            IUniswapV3Pool(
+                PoolAddress.computeAddress(
+                    nft.factory(),
+                    PoolAddress.PoolKey({token0: token0, token1: token1, fee: fee})
+                )
+            )
+                .positions(PositionKey.compute(address(nft), tickLower, tickUpper));
+
+        amount0 = uint256(tokensOwed0);
+        amount1 = uint256(tokensOwed1);
+
+        amount0 += FullMath.mulDiv(
+            poolFeeGrowthInside0LastX128 - positionFeeGrowthInside0LastX128,
+            liquidity,
+            FixedPoint128.Q128
+        );
+
+        amount1 += FullMath.mulDiv(
+            poolFeeGrowthInside1LastX128 - positionFeeGrowthInside1LastX128,
+            liquidity,
+            FixedPoint128.Q128
+        );
     }
 }
