38a6f107bc323e694b5b9fe219f7e7b81736aeb1
Emily Williams <emag3m@gmail.com>
2021-06-23 11:00:55 -0600

    use existing lib for calculations

--- contracts/libraries/PositionValue.sol
+++ contracts/libraries/PositionValue.sol
@@ -1,57 +1,33 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity >=0.7.0;
 
-import '@openzeppelin/contracts/math/SafeMath.sol';
-import '@uniswap/v3-core/contracts/libraries/FixedPoint96.sol';
-import '@uniswap/v3-core/contracts/libraries/FullMath.sol';
+import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
+import '@uniswap/v3-core/contracts/libraries/TickMath.sol';
 import '../interfaces/INonfungiblePositionManager.sol';
+import './LiquidityAmounts.sol';
+import './PoolAddress.sol';
 
 library PositionValue {
-    using SafeMath for uint256;
+    function principal(
+        address factory,
+        INonfungiblePositionManager nft,
+        uint256 tokenId
+    ) internal view returns (uint256 amount0, uint256 amount1) {
+        (, , address token0, address token1, uint24 fee, int24 tickLower, int24 tickUpper, uint128 liquidity, , , , ) =
+            nft.positions(tokenId);
 
-    struct PrincipalParams {
-        uint160 sqrtRatioX96;
-        uint160 sqrtRatioX96Lower;
-        uint160 sqrtRatioX96Upper;
-        int128 liquidity;
-    }
+        (uint160 sqrtRatioX96, , , , , , ) =
+            IUniswapV3Pool(
+                PoolAddress.computeAddress(factory, PoolAddress.PoolKey({token0: token0, token1: token1, fee: fee}))
+            )
+                .slot0();
 
-    function principal(PrincipalParams calldata params) internal pure returns (uint256 amount0, uint256 amount1) {
-        require(params.sqrtRatioX96Lower < params.sqrtRatioX96Upper, 'Bad Range');
-
-        if (params.sqrtRatioX96 < params.sqrtRatioX96Lower) {
-            amount0 = calculateAmount0(
-                uint256(params.liquidity) << FixedPoint96.RESOLUTION,
-                params.sqrtRatioX96Lower,
-                params.sqrtRatioX96Upper
+        return
+            LiquidityAmounts.getAmountsForLiquidity(
+                sqrtRatioX96,
+                TickMath.getSqrtRatioAtTick(tickLower),
+                TickMath.getSqrtRatioAtTick(tickUpper),
+                liquidity
             );
-            amount1 = 0;
-        } else if (params.sqrtRatioX96 < params.sqrtRatioX96Upper) {
-            amount0 = calculateAmount0(
-                uint256(params.liquidity) << FixedPoint96.RESOLUTION,
-                params.sqrtRatioX96,
-                params.sqrtRatioX96Upper
-            );
-            amount1 = calculateAmount1(params.liquidity, params.sqrtRatioX96Lower, params.sqrtRatioX96);
-        } else {
-            amount0 = 0;
-            amount1 = calculateAmount1(params.liquidity, params.sqrtRatioX96Lower, params.sqrtRatioX96Upper);
-        }
-    }
-
-    function calculateAmount0(
-        uint256 liquidityX96,
-        uint160 lowerBound,
-        uint160 upperBound
-    ) private pure returns (uint256 amount0) {
-        amount0 = liquidityX96.mul(upperBound - lowerBound).div(upperBound).div(lowerBound);
-    }
-
-    function calculateAmount1(
-        uint256 liquidity,
-        uint160 lowerBound,
-        uint160 upperBound
-    ) private pure returns (uint256 amount1) {
-        amount1 = FullMath.mulDiv(liquidity, (upperBound - lowerBound), FixedPoint96.RESOLUTION);
     }
 }
