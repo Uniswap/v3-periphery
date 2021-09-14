cf0c5960e5535672ea84678426fa4292a44f81f1
Alice Henshaw <henshawalice@gmail.com>
2021-09-14 15:57:18 -0400

    Adding test for overflowing timestamps

--- test/OracleLibrary.spec.ts
+++ test/OracleLibrary.spec.ts
@@ -301,5 +301,29 @@ describe('OracleLibrary', () => {
       // run test
       await runObservationsTest(blockTimestamps, initializeds, observationCardinality, observationIndex)
     })
+
+    it('fetches the correct timestamp when the timestamps overflow', async () => {
+      const maxUint32 = (2**32)-1
+      const blockTimestamps = [maxUint32, 3, maxUint32 - 2, 0]
+      const initializeds = [true, true, true, false]
+      const observationCardinality = 3
+      const observationIndex = 1
+      const mockObservations = await mockObservationsFactory.deploy(
+        blockTimestamps,
+        initializeds,
+        observationCardinality,
+        observationIndex
+      )
+
+      var result = await oracle.getOldestObservationSecondsAgo(mockObservations.address)
+
+      //calculate seconds ago
+      var secondsAgo = result['currentTimestamp'] - blockTimestamps[(observationIndex + 1) % observationCardinality]
+      if (secondsAgo < 0) {
+        secondsAgo += (2**32)
+      }
+
+      expect(result['secondsAgo']).to.equal(secondsAgo)
+    })
   })
 })
