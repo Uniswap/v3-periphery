b5ef56e8f5f06666739f8501342db7a6c80317af
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-01 17:36:03 +0800

    Delete test directory

--- test/shared/extractJSONFromURI.ts
+++
@@ -1,5 +0,0 @@
-export function extractJSONFromURI(uri: string): { name: string; description: string; image: string } {
-  const encodedJSON = uri.substr('data:application/json;base64,'.length)
-  const decodedJSON = Buffer.from(encodedJSON, 'base64').toString('utf8')
-  return JSON.parse(decodedJSON)
-}
