44d7c4785e67d869ff988e54f5acbf80caedd8e9
sam bacha <sam@manifoldfinance.com>
2022-10-08 08:32:57 -0700

    fix(build): dist

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,13 @@
-import '@nomiclabs/hardhat-ethers'
-import '@nomiclabs/hardhat-etherscan'
-import '@nomiclabs/hardhat-waffle'
-import 'hardhat-typechain'
-import 'hardhat-watcher'
+// @ts-check
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+import '@nomiclabs/hardhat-ethers';
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-waffle';
+import 'hardhat-watcher';
 
 const LOW_OPTIMIZER_COMPILER_SETTINGS = {
   version: '0.7.6',
@@ -93,6 +98,10 @@ export default {
       'contracts/NonfungibleTokenPositionDescriptor.sol': LOWEST_OPTIMIZER_COMPILER_SETTINGS,
       'contracts/libraries/NFTDescriptor.sol': LOWEST_OPTIMIZER_COMPILER_SETTINGS,
     },
+  },
+  typechain: {
+    outDir: 'types/',
+    target: 'ethers-v5',
   },
   watcher: {
     test: {
