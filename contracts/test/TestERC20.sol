bed37832a4b285b01816519b2c518a07407272d9
Pablo Urriza <pablo.urriza@gmail.com>
2025-03-31 17:13:14 +0200

    Force changes to relevant files. Some files were changed due to TransferHelper needed to be renamed due to collisions with v3-core

--- contracts/test/TestERC20.sol
+++ contracts/test/TestERC20.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: UNLICENSED 
 pragma solidity =0.7.6;
 
 import '@openzeppelin/contracts/drafts/ERC20Permit.sol';
--- contracts/test/UniswapV3SwapDynamicFee.t.sol
+++ contracts/test/UniswapV3SwapDynamicFee.t.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: BUSL-1.1 
 pragma solidity =0.7.6;
 pragma abicoder v2;
 
